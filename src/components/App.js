cf0b83d8eb1d163b88e7182f8f4b0c9e23eb76ad
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-02-05 12:46:50 +0530

    mov

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ const App = () => {
     <div id="main">
       <h1 data-ns-test="project-name">Movie Database</h1>
       <p data-ns-test="project-description">
-        {" "}
         descriptions of movies and allows visually impaired people to follow a
         movie along with their peers
       </p>
